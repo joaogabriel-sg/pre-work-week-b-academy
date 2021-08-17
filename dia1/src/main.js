2a8470428c47478989eef7ee40507859a07e74b9
João Gabriel <main.joaogabrielsg@gmail.com>
2021-08-17 19:49:34 -0300

    feat: add app toggle visibility

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,5 +1,27 @@
 import "./style.css";
 
+const app = document.querySelector('[data-js="app"]');
+const buttonLink = document.querySelector('[data-js="link"]');
+
+function handleToggleAppVisibility(event) {
+  event.preventDefault();
+
+  /*
+    Optei por utilizar a abordagem com visibility
+    por ter colocado o botão abaixo da div .app e por
+    por ele não influenciar no posicionamento do botão
+    ou quaisquer outros elementos quando a app não for visível
+    em tela, ou seja, no meu caso, o botão não subiria.
+  */
+
+  const appVisibility = getComputedStyle(app).visibility;
+  const isAppVisible = appVisibility === "visible";
+
+  app.style.visibility = isAppVisible ? "hidden" : "visible";
+}
+
+buttonLink.addEventListener("click", handleToggleAppVisibility, false);
+
 document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
