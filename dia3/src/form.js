11b8121baadc1f5b266b67522e2907612c7f2166
João Gabriel <main.joaogabrielsg@gmail.com>
2021-08-19 11:42:52 -0300

    feat: add select element to choose colors

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,4 +1,30 @@
+const colors = [
+  { name: "Vermelho", hexCode: "#e74c3c" },
+  { name: "Verde", hexCode: "#2ecc71" },
+  { name: "Azul", hexCode: "#3498db" },
+  { name: "Preto", hexCode: "#191919" },
+  { name: "Amarelo", hexCode: "#f1c40f" },
+  { name: "Laranja", hexCode: "#e67e22" },
+];
+
+const form = document.querySelector('[data-js="form"]');
 const inputName = document.querySelector('[data-js="input-name"]');
+
+const colorsSelect = document.createElement("select");
+colorsSelect.setAttribute("multiple", true);
+colors.forEach((color) => {
+  const option = document.createElement("option");
+  option.value = color.hexCode;
+  option.textContent = color.name;
+
+  colorsSelect.appendChild(option);
+});
+
+form.appendChild(colorsSelect);
+
+const divSelectedColorsContainer = document.createElement("div");
+divSelectedColorsContainer.classList.add("selected-color-container");
+colorsSelect.insertAdjacentElement("afterend", divSelectedColorsContainer);
 
 function handleFormatInputName(event) {
   const unformattedName = event.target.value;
@@ -21,4 +47,19 @@ function handleFormatInputName(event) {
   event.target.value = formattedName;
 }
 
+function handleShowSelectedColors(event) {
+  const selectedOptions = [...event.target.selectedOptions];
+
+  divSelectedColorsContainer.innerHTML = "";
+
+  selectedOptions.forEach((option) => {
+    const hexColorCode = option.value;
+    const selectedColorDiv = document.createElement("div");
+    selectedColorDiv.style.background = hexColorCode;
+
+    divSelectedColorsContainer.appendChild(selectedColorDiv || "#ccc");
+  });
+}
+
 inputName.addEventListener("input", handleFormatInputName);
+colorsSelect.addEventListener("change", handleShowSelectedColors);
